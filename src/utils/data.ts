be65d4a336f881deb8afe824d9fd74f19c890cfe
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-05 22:01:37 -0600

    make some changes

---
+++ src/utils/data.ts
@@ -0,0 +1,20 @@
+const services = [
+  {
+    title:"Mito 1",
+    description:"Las perspmas mayores son incapaces de aprender cosas nuevas"
+  },
+  {
+    title:"Mito 2",
+    description:"Todas las personas mayores tienen problemas de salud graves"
+  },
+  {
+    title:"Mito 3",
+    description:"Pierden el interés en la vida social y entretenimiento"
+  },
+  {
+    title:"Mito 4",
+    description:"El envejecimiento implíca inevitablemente deterioro cognitivo severo"
+  },
+]
+
+export { services }
